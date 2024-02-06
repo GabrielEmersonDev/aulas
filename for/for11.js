d4995a0d43591f2e34408874220b10ed1b1fb0c4
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-02-06 17:55:54 -0300

    feat: praticando

---
+++ for/for11.js
@@ -0,0 +1,13 @@
+// Escreva um programa em JavaScript que calcule a soma dos quadrados dos números pares de 1 a 1000. Utilize um loop for para iterar de 1 a 1000 e,
+//  dentro do loop, verifique se o número é par. Se for par, adicione o quadrado do número à soma.
+
+let soma = 0;
+
+for (let i = 0; i <= 1000; i++) {
+  if (i % 2 == 0) {
+    soma = soma + i * i;
+    console.log(i);
+  }
+}
+
+console.log(soma);
