3d0d5c92a529356d3c3ad28fb493bcd04e9e0ed3
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-02-08 02:18:59 -0300

    feat: praticando

--- while/while4.js
+++ while/while4.js
@@ -4,3 +4,31 @@
 //   O programa deve solicitar ao jogador que lance o dado (simbolizado por um número aleatório entre 1 e 6)
 //    e exiba o número obtido em cada lance, além do total de pontos acumulados. O jogo continua até que o jogador tire um 1 ou decida parar.
 //     Se o jogador decidir parar, o programa deve exibir o total de pontos acumulados.
+
+const jogadas = [4, 6, 4, 1, 4];
+let soma = 0;
+let jogar = true;
+contador = 0;
+
+while (jogar || contador < jogadas.length) {
+  contador += 1;
+  if ((contador = jogadas.length)) {
+    jogar = false;
+  }
+
+  for (let i = 0; i < jogadas.length; i++) {
+    console.log("Resultado de Lançamento de dado:", jogadas[i]);
+    if (jogadas[i] == 1) {
+      jogar = false;
+      soma = 0;
+      console.log(
+        "Pontuação total zerada, usuário acabou rolando número 1. Soma:",
+        soma
+      );
+      break;
+    } else {
+      soma += jogadas[i];
+      console.log("Total de pontos acumulados:", soma);
+    }
+  }
+}
