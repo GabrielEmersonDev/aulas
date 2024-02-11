789dad509801e717c9fb79209aacb1590483e1af
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-02-11 15:22:45 -0300

    feat: praticando

---
+++ questõesM/questão1/questaoTS/questao1TS.ts
@@ -0,0 +1,39 @@
+// // Exercício do sextou: Identificação de Valores Falsy
+
+// // Objetivo: Crie uma função em JavaScript que
+// // recebe uma array de elementos e retorna
+// //  a quantidade de valores falsy presentes nesse array
+
+// // Observação:
+
+// // const arrayExemplo = [0, false, '', undefined, null, NaN, 42, 'Hello', true];
+// // Depois de terminar se quiser mais dificuldad, faz a mesma coisa só que usando typescript
+// // Depois de terminar o typescript pode refazer agora usando filter
+
+// // ! nega
+
+// function acharFalsy(array: any[]): number {
+//   let contadorFalsy: number = 0;
+
+//   for (let i: number = 0; i < array.length; i++) {
+//     if (!array[i]) {
+//       contadorFalsy++;
+//     }
+//   }
+//   return contadorFalsy;
+// }
+
+// const arrayExemplo: any[] = [
+//   0,
+//   false,
+//   "",
+//   undefined,
+//   null,
+//   NaN,
+//   42,
+//   "Hello",
+//   true,
+// ];
+
+// const totalFalsy: number = acharFalsy(arrayExemplo);
+// console.log(totalFalsy);
