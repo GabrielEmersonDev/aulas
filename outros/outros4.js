8d9e032e510d7eeae8823172bc536baa3bc65d33
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-02-13 01:19:04 -0300

    feat: praticando

---
+++ outros/outros4.js
@@ -0,0 +1,19 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+= (atribuição)
+== igualdade   (valor )  ( não recomendado usar)
+=== igualdade estrita (valor e tipo)
+!= diferente     (valor ) ( n usa)
+!== diferente estrito ( valor e tipo)
+*/
+
+// console.log(10 > 5);
+
+const num1 = 10; //number
+const num2 = "10"; //number
+const comp = num1 !== num2;
+console.log(comp);
