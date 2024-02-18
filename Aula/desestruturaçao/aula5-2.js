ea718507f5f4f69b3670e7d4575b6f7c0818414d
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-02-18 13:38:27 -0300

    feat: praticando

---
+++ Aula/desestruturaçao/aula5-2.js
@@ -0,0 +1,33 @@
+// Agora, sua tarefa é criar uma função chamada encontrarLivroPorAno que recebe o array livros e
+// um ano como parâmetro e retorna o título do livro publicado naquele ano e seu ano(treinar objeto). Se nenhum livro for
+// encontrado para o ano fornecido, a função deve retornar uma mensagem indicando que nenhum
+// livro foi encontrado.
+
+const livros = [
+  {
+    titulo: "Aventuras de Sherlock Holmes",
+    autor: "Arthur Conan Doyle",
+    ano: 1892,
+  },
+  { titulo: "1984", autor: "George Orwell", ano: 1949 },
+  { titulo: "Cem Anos de Solidão", autor: "Gabriel García Márquez", ano: 1967 },
+];
+function encontrarLivroPorAno(livro, anoProcurado) {
+  const livrosEncontrados = [];
+
+  for (let i = 0; i < livro.length; i++) {
+    if (anoProcurado === livro[i].ano) {
+      livrosEncontrados.push({ titulo: livro[i].titulo, ano: livro[i].ano });
+    }
+  }
+
+  if (livrosEncontrados.length !== 0) {
+    for (let i = 0; i < livrosEncontrados.length; i++) {}
+    return livrosEncontrados;
+  } else {
+    return "Não foram encontrados livros";
+  }
+}
+
+const livrosEncontrados = encontrarLivroPorAno(livros, 1967);
+console.log(livrosEncontrados);
---
+++ Aula/desestruturaçao/aula6.js
@@ -0,0 +1,34 @@
+// A sua tarefa é criar uma função chamada encontrarFilmePorDiretor que recebe o array de filmes e
+// um nome de diretor como parâmetro. A função deve retornar um array com os títulos dos filmes dir
+// igidos pelo diretor fornecido. Se nenhum filme for encontrado para o diretor fornecido, a função
+// deve retornar uma mensagem indicando que nenhum filme foi encontrado.
+
+const filmes = [
+  { titulo: "Matrix", diretor: "Lana Wachowski", ano: 1999 },
+  { titulo: "Pulp Fiction", diretor: "Quentin Tarantino", ano: 1994 },
+  { titulo: "Inception", diretor: "Christopher Nolan", ano: 2010 },
+  { titulo: "The Shawshank Redemption", diretor: "Frank Darabont", ano: 1994 },
+];
+
+function encontrarFilmePorDiretor(array, diretor) {
+  const filmesEncontrados = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (diretor === array[i].diretor) {
+      filmesEncontrados.push({
+        titulo: array[i].titulo,
+        diretor: array[i].diretor,
+        ano: array[i].ano,
+      });
+    }
+  }
+
+  if (filmesEncontrados.length !== 0) {
+    return filmesEncontrados;
+  } else {
+    return `Não foram encontrados filmes com o diretor ${diretor}`;
+  }
+}
+
+const filmesEncontrados = encontrarFilmePorDiretor(filmes, "Frank Darabont");
+console.log(filmesEncontrados);
